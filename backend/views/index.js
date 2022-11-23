12330f97f809a6368781aeca4ad0a6a37c0a6d83
Kai Brown <kaibrown28@outlook.com>
2022-11-23 00:01:25 -0500

    created index.js

---
+++ backend/views/index.js
@@ -0,0 +1,46 @@
+//------requirements and dependencies---------//
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries')
+const app = express()
+
+//redux dependencies
+// const webpack = require('webpack')
+// const webpackDevMiddleware = require('webpack-dev-middleware')
+// const webpackHotMiddleware = require('webpack-hot-middleware')
+// const config = require('./webpack.config')
+
+//variables
+const port = 3000
+
+//--------MIDDLEWARE-------------//
+//body parser extracts the entire body portion of an incoming request
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended:true,
+    })
+)
+//redux middleware
+// const compilier = webpack(config)
+// app.use(webpackDevMiddleware(compilier, {noInfo: true, publicPath: config.output.publicPath}))
+// app.use(webpackHotMiddleware(compilier))
+//root endpoint
+
+//write a test message in this response
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+  //listeners
+app.listen(port, () => {
+    console.log(`The app is listening on port ${port}.`)
+})
+
+//setting http requests for a table
+app.get('/request', db.getRequest)
+app.get('/request/:id', db.getRequestById)
+app.post('/request/', db.CreateRequest)
+app.put('/request/:id', db.updateRequest)
+app.delete('/request/:id', db.deleteRequest)
