2143d52810e6e0c2a8315ea1fa9d9e55c2947b4e
Kai Brown <kaibrown28@outlook.com>
2022-11-28 18:10:13 -0500

    further database work

--- backend/server/database.sql
+++ backend/server/database.sql
@@ -1,8 +1,10 @@
+-- student database
 CREATE DATABASE students;
 
 CREATE TABLE student(
     student_id SERIAL PRIMARY KEY,
     name VARCHAR (50),
+    image VARCHAR(500),
     email VARCHAR(50),
     gender boolean NULL,
     age int,
@@ -14,9 +16,19 @@ CREATE TABLE student(
     hasParticipated VARCHAR(255),
     awards VARCHAR(255),
     income VARCHAR(255),
-    essay varchar(500) 
+    essay varchar(500),
+    skills VARCHAR(255),
+    aboutMe VARCHAR(255) 
 );
 
+CREATE TABLE projects(
+    project_id SERIAL PRIMARY KEY,
+    description VARCHAR(255),
+    video VARCHAR(500),
+
+);
+
+-- company database
 CREATE DATABASE companies;
 CREATE TABLE company(
     company_id SERIAL PRIMARY KEY,
@@ -29,12 +41,15 @@ CREATE TABLE company(
     expertise VARCHAR(255),
     description VARCHAR(255),
     hasOpportunities boolean NULL,
+    rating int,
 );
 
 CREATE TABLE jobs(
     job_id SERIAL PRIMARY KEY,
     description VARCHAR(255),
     pointOfContact VARCHAR(50),
-    skillsNeeded VARCHAR(50)
-
+    skillsNeeded VARCHAR(50),
 );
+
+-- teacher database
+-- school database
