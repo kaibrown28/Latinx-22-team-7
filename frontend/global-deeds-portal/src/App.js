b11fecdfca88e281b71f19692e2e7d35beaa04fc
Kai Brown <104911237+kaibrown28@users.noreply.github.com>
2022-12-09 16:17:08 -0500

    fixed routing (react router 6, freecodebootcamp tutorial)

--- frontend/global-deeds-portal/src/App.js
+++ frontend/global-deeds-portal/src/App.js
@@ -1,11 +1,19 @@
-import { PageRoutes } from './Routes';
-import './index.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Home} from './pages/Home'
+import { LogInPage } from './pages/LogInPage'
+import { TestPage } from './pages/test'
+import { StudentProfilePage } from './pages/StudentProfilePage'
 
 export const App = () => {
     return (
-        <div className="page-container">
-            <PageRoutes />
-            <h1>Huh?</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path="/login" element={<LogInPage />} />
+                <Route path="/test" element={<TestPage />} />
+                <Route path="/studentprofile" element={<StudentProfilePage />} />
+                <Route path="testing" element={<div><h2>testing</h2></div>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
---
+++ frontend/global-deeds-portal/src/pages/Home.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const Home = () => {
+
+
+return (
+    <div>
+        Are you working?
+        Welcome to the portal
+    </div>
+)
+};
