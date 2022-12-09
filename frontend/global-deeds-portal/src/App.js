d26b44e6f20ca59e2a8c57787b61d289dbeb5fff
Kai Brown <104911237+kaibrown28@users.noreply.github.com>
2022-12-09 14:31:32 -0500

    trying out react router tutorial steps

--- frontend/global-deeds-portal/src/App.js
+++ frontend/global-deeds-portal/src/App.js
@@ -1,11 +1,29 @@
-import { PageRoutes } from './Routes';
-import './index.css'
+import { Route, Routes, Link } from "react-router-dom"
+import { StudentProfilePage } from './pages/StudentProfilePage';
+import { LogInPage } from './pages/LogInPage';
+import { TestPage } from './pages/test';
+import { Home } from './pages/home';
+
 
 export const App = () => {
     return (
-        <div className="page-container">
-            <PageRoutes />
-            <h1>Huh?</h1>
-        </div>
+        <>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home Page</Link></li>
+                    <li><Link to="/studentprofile">Student Profile</Link></li>
+                    <li><Link to="/test">Test Page</Link></li>
+                    <li><Link to="/login">Log In</Link></li>
+                    
+                </ul>
+            </nav>
+            <div>Am I dreaming?</div>
+            <Routes>
+                <Route path="/"  element={<Home />} />
+                <Route path="/studentprofile" element={<StudentProfilePage />} />
+                <Route path="/login" element={<LogInPage />} />  
+                <Route path="/test" element={<TestPage />} />  
+            </Routes>
+        </>
     );
 }
---
+++ frontend/global-deeds-portal/src/pages/home.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const Home = () => {
+
+
+return (
+    <div>
+        This is the home page.
+    </div>
+)
+};
